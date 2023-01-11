f50e99e9fe13f65479bcdbe0ed17e1e382c72046
ChrisMendoza99 <78059716+shiro-no-karisu@users.noreply.github.com>
2023-01-10 22:26:01 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,135 +1,103 @@
-//By: Christopher Mendoza
-
 #include "msp.h"
-#include "lcdLib_432.h"
+#define ONE_SEC 2130000 //becasue im lazy and wanted to be cool
+//The pointer is used to change the value of the constant integer
 
-double gas = 0.0;
-double MAX = 5.0;
-int cost;
 
-void update()
+void toaster_one()
 {
-    lcdClear();
-    lcdSetText("Total Gas: ", 0, 0);
-    lcdSetDouble(gas, 12, 0);
 
-
+    P2->OUT = BIT1;
+    __delay_cycles(4260000); //delayed for 2 seconds
+    P2->OUT &= ~BIT1;
+    P2->OUT = 0x0C;
+    __delay_cycles(ONE_SEC); //delayed for 2 seconds
+    P2->OUT &= ~0x0C;
 }
-void reset()
+void toaster_two()
 {
-    P5->OUT &= ~BIT2;
-    P5->OUT &= ~BIT1;
-    P5->OUT &= ~BIT0;
-    lcdSetText("Select Octane",1,0);
-    lcdSetText("87  89  93", 2, 1);
+    P2->OUT = BIT1;
+    __delay_cycles(6390000); //delayed for 2 seconds
+    P2->OUT &= ~BIT1;
+    P2->OUT = 0x0C;
+    __delay_cycles(ONE_SEC); //delayed for 2 seconds
+    P2->OUT &= ~0x0C;
+}
+void toaster_three()
+{
+
+    P2->OUT = BIT1;
+    __delay_cycles(8520000); //delayed for 2 seconds
+    P2->OUT &= ~BIT1;
+    P2->OUT = 0x0C;
+    __delay_cycles(ONE_SEC); //delayed for 2 seconds
+    P2->OUT &= ~0x0C;
+}
+void toaster_four()
+{
+
+    P2->OUT = BIT1;
+    __delay_cycles(10650000); //delayed for 2 seconds
+    P2->OUT &= ~BIT1;
+    P2->OUT = 0x0C;
+    __delay_cycles(ONE_SEC); //delayed for 2 seconds
+    P2->OUT &= ~0x0C;
 }
 
-void pressed()
+
+void PORTINIT()
 {
-    update();
-    gas = 0;
-    while(gas < MAX)
+    /*Input Interrupt*/
+    P2->DIR = 0xF0; //Setting as Inputs
+    P2->REN = 0xF0; //Enabling Resistors as pull
+    P2->OUT = 0xF0; //P4.0 -> P4.3 are set as Pull Up
+    P2->IES = 0xF0; //Detects on the falling edge
+    P2->IFG = 0; //flag clear
+    P2->IE = 0xF0;
+    NVIC->IP[36] = 0x09;
+    NVIC->ISER[1] |= BIT4;
+    /*Outputs*/
+    P2->DIR = 0x0E; //Setting P2.3 and P2.4 as Outputs
+    P2->OUT = 0x0E;
+    P2->OUT &= ~0x0E;
+}
+
+void PORT2_IRQHandler()
+{
+    if(P2->IN & BIT4){}
+    else
     {
-        if((P2->IN & BIT6) == 0)
-        {
-            delay_ms(100);
-            gas+=0.100;
-            update();
-
-        }
+        toaster_one();
     }
-    while(gas > MAX)
-         {
-             if((P2->IN & BIT6) == 0){}
-             else
-             {
-                 delay_ms(100);
-                 gas+=0.100;
-                 update();
-
-             }
-             if(P2->IN & BIT7)
-             {
-                 return;
-             }
-         }
-
-
-
-
-}
-
-
-void GPIOInit()
-{
-    /*Input PORTS*/
-    P2->DIR |= 0x07;
-    P2->REN |= 0xF8;
-    P2->OUT |= 0xFF;
-    P2->OUT &= ~0x07;
-    /*LED Ports*/
-    P5->DIR = 0x07;
-    P5->OUT |= 0x07;
-    P5->OUT &= ~0x07;
-}
-
-int main(void)
-{
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-    /*InitStage*/
-    GPIOInit();
-    /*LCD greetings*/
-    lcdInit();
-    lcdClear();
-    lcdSetText("Select Octane",1,0);
-    lcdSetText("87  89  93", 2, 1);
-
-
-    while(1)
+    if(P2->IN & BIT5){}
+    else
     {
-        if(P2->IN & 0x08){}
-        else
-        {
-            P5->OUT |= BIT0;
-            lcdClear();
-            lcdSetText("87 selected",0,0);
-            delay_ms(3000);
-            pressed();
-        }
-
-        if(P2->IN & 0x10){}
-        else
-        {
-            P5->OUT |= BIT1;
-            lcdClear();
-            lcdSetText("89 selected",0,0);
-            delay_ms(3000);
-            pressed();
-        }
-        if(P2->IN & 0x20){}
-        else
-        {
-            P5->OUT |= BIT2;
-            lcdClear();
-            lcdSetText("93 selected",0,0);
-            delay_ms(3000);
-            pressed();
-        }
-        if(P2->IN & 0x80){}
-        else
-        {
-            lcdClear();
-             lcdSetText("Have a Good Day", 0, 0);
-             delay_ms(3000);
-             lcdClear();
-             reset();
-        }
-
+        toaster_two();
     }
-
-
+    if(P2->IN & BIT6){}
+    else
+    {
+        toaster_three();
+    }
+    if(P2->IN & BIT7){}
+    else
+    {
+        toaster_four();
+    }
+    //SysTick->CTRL &= ~(SysTick_CTRL_ENABLE_Msk);
+    P2->IFG = 0;
 }
 
 
+void main(void)
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    PORTINIT();
+    //SysTick_Init();
+    __enable_irq();// Enable global interrupt
+        //While there is no decision making, the device will be asleep
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+    SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
+    __DSB();
+    __WFI();
 
-
+}
